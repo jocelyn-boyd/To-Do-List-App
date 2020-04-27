d390173894ccb2ca966623dbd82a4ea75305fdf9
Jocelyn Boyd <jocelynboyd@pursuit.org>
2020-04-26 22:08:53 -0400

    Added a DataSource

--- ToDoList/TasksController.swift
+++ ToDoList/TasksController.swift
@@ -13,8 +13,18 @@ class TasksController: UITableViewController {
   
 }
 
-// MARK: - DataSource
+// MARK: Data Source
 extension TasksController {
   
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 10
+  }
+  
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    cell.textLabel?.text = "Task"
+    return cell
+  }
   
 }
